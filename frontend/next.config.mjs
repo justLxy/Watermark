27479b7fa8eade37d2cbe3bb29a2749cf9c2b35f
Xuanyi Lyu <xuanyi.lyu@mail.utoronto.ca>
2025-07-10 01:13:44 +0800

    Deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,7 @@
 /* eslint-disable */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 跳过构建阶段的 ESLint，以避免部署因样式/语法警告失败
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+    output: 'standalone',
 };
 
 export default nextConfig;
